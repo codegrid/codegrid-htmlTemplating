1fdebd5d78f7aa357b0ae08ea9f909cf9e70dbcd
Takeshi Takatsudo <takazudo@gmail.com>
2012-11-05 22:43:37 +0900

    added sample1

---
+++ sample1/js/main.js
@@ -0,0 +1,7 @@
+$.ui.domwindowdialog.setup({
+  width: 600, // dialog width
+  height: 300, // dialog height
+  spinjs: true, // make this true to use spin.js
+  selector_open: '.apply-domwindow-open', // opener selector
+  selector_close: '.apply-domwindow-close' // closer selector
+});
